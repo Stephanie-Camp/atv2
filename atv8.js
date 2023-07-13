a3bc749be919b5f61e27c2c4cbb303945fe7dda8
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-07-13 23:56:39 +0000

    att

--- atv8.js
+++ atv8.js
@@ -1,11 +1,10 @@
 var v = [1, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 1, 4, 3, 0, 1, 4, 5, 6, 7, 8, 6]
-
-var x = v.length-1;
 var value = 2;
 
 while(true){
+  
   var num = v.indexOf(value);
-
+  
   if(num === -1){
     break;
   }
--- atv8_3.js
+++ atv8_3.js
@@ -1,17 +1,22 @@
 var v = [5, 1, 3, 8, 1, 4, 5, 2, 0, 7, 6];
 var x = v.length;
-var num = 0;
+var num;
 var vet = [];
+var i = 0;
 
 for(var i = 0; i < x; i++){
-  while(v[i] < v[i+1]) {
+  while(v[i] > v[i+1] && i != -1){
     num = v[i];
     v[i] = v[i+1]
     v[i+1] = num
-    if (v[i] < v[i]) {
-      vet.splice(0, 1, num);
-    }
-  }
-}
+    if(v[i] < v[i+1]){
+      vet.splice(v[i], 1); 
 
-console.log(`Vetor organizado por ordem crescente: `, v, "\nVETOR VET", vet, num);
+    }
+    console.log(`\nValor v[i]: ${v[i]}------Valor num: ${num}\nVetor organizado por ordem crescente: `, v, "\n\nVETOR VET", vet);
+
+  }  
+}
+  
+
+
